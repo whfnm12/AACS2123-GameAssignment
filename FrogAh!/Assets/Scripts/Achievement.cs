b8a709bc07142434f7705189d533200d0d3eb54b
whfnm12 <whfnm12@gmail.com>
2022-09-28 16:17:23 +0800

    achievement menu ( bugged )

---
+++ FrogAh!/Assets/Scripts/Achievement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Achievement : MonoBehaviour
+{
+    private Image img;
+    
+    public enum AchievementTypes { babyJump, dummy2, dummy3 };
+    
+    [SerializeField] private AchievementTypes achievementType;
+    public AchievementTypes _achievementType { get { return achievementType; } }
+
+    [SerializeField] private string TrophyName;
+    [SerializeField] private string TrophyDesc;
+
+
+    public bool isUnlocked { get; private set; }
+
+    public void Awake()
+    {
+        img = GetComponent<Image>();
+        CheckIfAchievementIsUnlocked();
+    }
+    public void CheckIfAchievementIsUnlocked()
+    {
+        if (PlayerPrefs.GetInt(achievementType.ToString()) == 0)
+        {
+            img.color = Color.black;
+        }
+        else
+        {
+            img.color = Color.white;
+            isUnlocked = true;
+        }
+    }
+
+    public void UnlockThisAchievement()
+    {
+        PlayerPrefs.SetInt(achievementType.ToString(), 1);
+        CheckIfAchievementIsUnlocked();
+    }
+
+    public void OnTouchTrophy()
+    {
+        AchievementManager.Instance.UpdateTrophyTextsUI(TrophyName, TrophyDesc);
+    }
+}
---
+++ FrogAh!/Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AchievementManager : MonoBehaviour
+{
+    public static AchievementManager Instance;
+
+    [SerializeField] private TextMeshProUGUI TrophyName;
+    [SerializeField] private TextMeshProUGUI TrophyDesc;
+
+    [SerializeField] private Achievement[] trophies;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void UnlockAchievement(Achievement.AchievementTypes achievementType)
+    {
+        Achievement achievementToUnlock = 
+            Array.Find(trophies, dummyTrophies => dummyTrophies._achievementType == achievementType);
+
+        if(achievementToUnlock == null)
+        {
+            Debug.LogWarning("Trophy not added with achievement: " + achievementType);
+            return;
+        }
+
+        if (!achievementToUnlock.isUnlocked)
+        {
+            achievementToUnlock.UnlockThisAchievement();
+        }
+    }
+
+    public void UpdateTrophyTextsUI(string TrophyName, string TrophyDesc)
+    {
+        TrophyName.text = TrophyName;
+        TrophyDesc.text = TrophyDesc;
+    }
+}
+    
